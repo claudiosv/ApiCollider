ae09d3a9151d057fc6ed2e50fe04e7e1550e08a7
Claudio Spiess <claudiosv@users.noreply.github.com>
2018-08-21 11:43:10 -0400

    Cleanup, fix gitignore

--- scrape.py
+++ scrape.py
@@ -43,16 +43,9 @@ def log_error(e):
 if __name__ == '__main__':
     for page in range(716):
         raw_html = simple_get('https://www.programmableweb.com/category/all/apis?page={}'.format(page))
-        #len(raw_html)
         client = MongoClient('mongodb://apicollider:apicollider123@ds125892.mlab.com:25892/apicollider')
- # r    aw_html = open('contrived.html').read()
         html = BeautifulSoup(raw_html, 'html.parser')
         table = html.find_all('tbody')[2].find_all('tr')
- #pr    int(table)
- #fo    r p in html.select('td'):
-         #print(p['class'])
-         #if 'views-field-title' in p['class']: #str.startswith
-          #   print(p.text)
         db = client.apicollider
         apic = db.apis
         pairs = []
@@ -64,39 +57,3 @@ if __name__ == '__main__':
                 print(title, description)
 
         new_result = apic.insert_many(pairs)
-    # print('Getting the list of names....')
-    # names = get_names()
-    # print('... done.\n')
-
-    # results = []
-
-    # print('Getting stats for each name....')
-
-    # for name in names:
-    #     try:
-    #         hits = get_hits_on_name(name)
-    #         if hits is None:
-    #             hits = -1
-    #         results.append((hits, name))
-    #     except:
-    #         results.append((-1, name))
-    #         log_error('error encountered while processing '
-    #                   '{}, skipping'.format(name))
-
-    # print('... done.\n')
-
-    # results.sort()
-    # results.reverse()
-
-    # if len(results) > 5:
-    #     top_marks = results[:5]
-    # else:
-    #     top_marks = results
-
-    # print('\nThe most popular mathematicians are:\n')
-    # for (mark, mathematician) in top_marks:
-    #     print('{} with {} pageviews'.format(mathematician, mark))
-
-    # no_results = len([res for res in results if res[0] == -1])
-    # print('\nBut we did not find results for '
-    #       '{} mathematicians on the list'.format(no_results))
